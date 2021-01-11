c258777a9a47d91527a40eb8eb0d8cd0a6d715a3
Harshit Srivastav <63945735+mr-procrastination@users.noreply.github.com>
2021-01-11 11:04:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/code/mr_procrastination/footballallover/ApiInterface.java
@@ -0,0 +1,16 @@
+package com.code.mr_procrastination.footballallover;
+
+import com.code.mr_procrastination.footballallover.Model.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+}
---
+++ app/src/main/java/com/code/mr_procrastination/footballallover/MainActivity.java
@@ -0,0 +1,64 @@
+package com.code.mr_procrastination.footballallover;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.code.mr_procrastination.footballallover.Model.Articles;
+import com.code.mr_procrastination.footballallover.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    final String API_KEY = "fbeabc0d32714b23bc29206a17e8c5b6";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String country = getCountry();
+        retrieveJson(country, API_KEY);
+    }
+
+    private void retrieveJson(String country, String api_key) {
+        Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country, api_key);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null){
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this, articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+}
